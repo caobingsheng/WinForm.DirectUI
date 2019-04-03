3205b10085cea7abddc475151aa60d31fd5da944
caobingsheng <caobingsheng@live.com>
2019-04-03 17:37:14 +0800

    clean code

--- test/WinForm.DirectUI.Demo/FrmDemo.cs
+++ test/WinForm.DirectUI.Demo/FrmDemo.cs
@@ -1,25 +1,25 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
 using System.Windows.Forms;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WinForm.DirectUI.Forms;
 
 namespace WinForm.DirectUI.Demo
 {
     public partial class FrmDemo : UIForm
     {
-        UIButton btnOut;
-        UIButton btnInner;
-        UIButton btnInner2;
-        UILabel label;
-        UIProgress progress;
-        UILine line;
-        UIImage image;
-        UIMarquee marquee;
-        UILink link;
-        UILink link2;
+        private UIButton btnOut;
+        private UIButton btnInner;
+        private UIButton btnInner2;
+        private UILabel label;
+        private UIProgress progress;
+        private UILine line;
+        private UIImage image;
+        private UIMarquee marquee;
+        private UILink link;
+        private UILink link2;
 
         public FrmDemo()
         {
@@ -28,7 +28,7 @@ namespace WinForm.DirectUI.Demo
             image = new UIImage();
             image.Dock = DockStyle.Right;
             image.Width = 400;
-            this.UIControls.Add(image);
+            UIControls.Add(image);
             //
             line = new UILine();
             line.Location = new Point(300, 270);
@@ -36,21 +36,21 @@ namespace WinForm.DirectUI.Demo
             line.LineBlendStyle = BlendStyle.FadeInFadeOut;
             line.LineWidth = 2;
             line.LineDashStyle = DashStyle.Dash;
-            this.UIControls.Add(line);
+            UIControls.Add(line);
             //
             label = new UILabel();
             label.Location = new Point(250, 200);
             label.Size = new Size(100, 50);
             label.Font = new Font("微软雅黑", 13f);
             label.Text = "测试文本测试文本测试文本测试文本测试文本";
-            this.UIControls.Add(label);
+            UIControls.Add(label);
             //
             progress = new UIProgress();
             progress.Location = new Point(300, 200);
             progress.Percentage = 20;
             progress.Size = new Size(600, 50);
             progress.Anchor = AnchorStyles.None;
-            this.UIControls.Add(progress);
+            UIControls.Add(progress);
             //
             btnOut = new UIButton();
             btnOut.Size = new Size(300, 100);
@@ -62,56 +62,56 @@ namespace WinForm.DirectUI.Demo
             btnOut.Font = new Font(btnOut.Font.FontFamily, 45f);
             btnOut.TextRenderingHint = TextRenderingHint.AntiAlias;
             btnOut.Click += (sender, e) => Console.WriteLine("单击out");
-            this.UIControls.Add(btnOut);
+            UIControls.Add(btnOut);
 
             //
             btnInner2 = new UIButton();
             btnInner2.Size = new Size(50, 20);
             btnInner2.Location = new Point(55, 55);
-            btnInner2.UIParent = this.btnOut;
+            btnInner2.UIParent = btnOut;
             btnInner2.Name = "in2";
             btnInner2.Text = btnInner2.Name;
             btnInner2.Click += (sender, e) => MessageBox.Show("单击in2");
-            this.btnOut.UIControls.Add(btnInner2);
+            btnOut.UIControls.Add(btnInner2);
             //
             btnInner = new UIButton();
             btnInner.Size = new Size(50, 20);
             btnInner.Location = new Point(5, 15);
-            btnInner.UIParent = this.btnOut;
+            btnInner.UIParent = btnOut;
             btnInner.Name = "in";
             btnInner.Text = btnInner.Name;
             btnInner.Click += (sender, e) => Console.WriteLine("单击in");
-            this.btnOut.UIControls.Add(btnInner);
+            btnOut.UIControls.Add(btnInner);
             //
             marquee = new UIMarquee();
             marquee.Size = new Size(801, 4);
             marquee.Location = new Point(100, 200);
             marquee.BorderColor = Color.Transparent;
-            this.marquee.BackColor = Color.Transparent;
+            marquee.BackColor = Color.Transparent;
             marquee.Dock = DockStyle.Bottom;
             marquee.ProgressColor = Color.DodgerBlue;
-            this.UIControls.Add(this.marquee);
-            this.marquee.SendToBack();
+            UIControls.Add(marquee);
+            marquee.SendToBack();
             //
             link = new UILink();
             link.Size = new Size(40, 15);
             link.Location = new Point(500, 10);
-            this.link.Font = new Font("微软雅黑", 10f, GraphicsUnit.Point);
+            link.Font = new Font("微软雅黑", 10f, GraphicsUnit.Point);
             link.Text = "换一张";
-            this.UIControls.Add(this.link);
+            UIControls.Add(link);
             //
             link2 = new UILink();
             link2.Size = new Size(40, 15);
             link2.Location = new Point(540, 10);
             link2.Text = "换一张";
-            this.UIControls.Add(this.link2);
+            UIControls.Add(link2);
         }
 
         public void TestCore(FormBorderStyle border, IUIWindow target)
         {
             //初始化
-            this.FormBorderStyle = border;
-            this.btnOut.UIParent = target;
+            FormBorderStyle = border;
+            btnOut.UIParent = target;
 
             //FindUIWindow
             Assert.AreEqual(btnOut.FindUIWindow(), target);
@@ -120,95 +120,95 @@ namespace WinForm.DirectUI.Demo
 
             //FindUIChild
             Assert.AreEqual(target.FindUIChild(Point.Empty), null);
-            Assert.AreEqual(target.FindUIChild(new Point(10, 20)), this.btnOut);
-            Assert.AreEqual(target.FindUIChild(new Point(109, 69)), this.btnOut);
-            Assert.AreEqual(target.FindUIChild(new Point(15, 35)), this.btnInner);
-            Assert.AreEqual(target.FindUIChild(new Point(64, 54)), this.btnInner);
+            Assert.AreEqual(target.FindUIChild(new Point(10, 20)), btnOut);
+            Assert.AreEqual(target.FindUIChild(new Point(109, 69)), btnOut);
+            Assert.AreEqual(target.FindUIChild(new Point(15, 35)), btnInner);
+            Assert.AreEqual(target.FindUIChild(new Point(64, 54)), btnInner);
             //FindUIChild
-            Assert.AreEqual(this.btnOut.FindUIChild(Point.Empty), null);
-            Assert.AreEqual(this.btnOut.FindUIChild(new Point(5, 15)), this.btnInner);
-            Assert.AreEqual(this.btnOut.FindUIChild(new Point(54, 34)), this.btnInner);
+            Assert.AreEqual(btnOut.FindUIChild(Point.Empty), null);
+            Assert.AreEqual(btnOut.FindUIChild(new Point(5, 15)), btnInner);
+            Assert.AreEqual(btnOut.FindUIChild(new Point(54, 34)), btnInner);
             //FindUIChild
-            Assert.AreEqual(target.FindUIChild("in2"), this.btnInner2);
+            Assert.AreEqual(target.FindUIChild("in2"), btnInner2);
             //PointToClient
             Point p = new Point(15, 35);//window client
             Point sp = target.PointToScreen(p);
             Assert.AreEqual(target.PointToClient(sp), p);
-            Assert.AreEqual(this.btnOut.PointToClient(sp), new Point(5, 15));
-            Assert.AreEqual(this.btnInner.PointToClient(sp), Point.Empty);
+            Assert.AreEqual(btnOut.PointToClient(sp), new Point(5, 15));
+            Assert.AreEqual(btnInner.PointToClient(sp), Point.Empty);
             //PointToScreen
             Assert.AreEqual(target.PointToScreen(p), sp);
-            Assert.AreEqual(this.btnOut.PointToScreen(new Point(5, 15)), sp);
-            Assert.AreEqual(this.btnInner.PointToScreen(Point.Empty), sp);
+            Assert.AreEqual(btnOut.PointToScreen(new Point(5, 15)), sp);
+            Assert.AreEqual(btnInner.PointToScreen(Point.Empty), sp);
             //PointToUIControl
             Assert.AreEqual(target.PointToUIControl(p), p);
-            Assert.AreEqual(this.btnOut.PointToUIControl(p), new Point(5, 15));
-            Assert.AreEqual(this.btnInner.PointToUIControl(p), Point.Empty);
+            Assert.AreEqual(btnOut.PointToUIControl(p), new Point(5, 15));
+            Assert.AreEqual(btnInner.PointToUIControl(p), Point.Empty);
             //PointToUIWindow
             Assert.AreEqual(target.PointToUIWindow(p), p);
-            Assert.AreEqual(this.btnOut.PointToUIWindow(new Point(5, 15)), p);
-            Assert.AreEqual(this.btnInner.PointToUIWindow(Point.Empty), p);
+            Assert.AreEqual(btnOut.PointToUIWindow(new Point(5, 15)), p);
+            Assert.AreEqual(btnInner.PointToUIWindow(Point.Empty), p);
             //RectangleToClient
             Rectangle r = new Rectangle(15, 35, 10, 20);//window client
             Rectangle sr = target.RectangleToScreen(r);
             Assert.AreEqual(target.RectangleToClient(sr), r);
-            Assert.AreEqual(this.btnOut.RectangleToClient(sr), new Rectangle(5, 15, 10, 20));
-            Assert.AreEqual(this.btnInner.RectangleToClient(sr), new Rectangle(0, 0, 10, 20));
+            Assert.AreEqual(btnOut.RectangleToClient(sr), new Rectangle(5, 15, 10, 20));
+            Assert.AreEqual(btnInner.RectangleToClient(sr), new Rectangle(0, 0, 10, 20));
             //RectangleToScreen
             Assert.AreEqual(target.RectangleToScreen(r), sr);
-            Assert.AreEqual(this.btnOut.RectangleToScreen(new Rectangle(5, 15, 10, 20)), sr);
-            Assert.AreEqual(this.btnInner.RectangleToScreen(new Rectangle(0, 0, 10, 20)), sr);
+            Assert.AreEqual(btnOut.RectangleToScreen(new Rectangle(5, 15, 10, 20)), sr);
+            Assert.AreEqual(btnInner.RectangleToScreen(new Rectangle(0, 0, 10, 20)), sr);
             //RectangleToUIControl
             Assert.AreEqual(target.RectangleToUIControl(r), r);
-            Assert.AreEqual(this.btnOut.RectangleToUIControl(r), new Rectangle(5, 15, 10, 20));
-            Assert.AreEqual(this.btnInner.RectangleToUIControl(r), new Rectangle(0, 0, 10, 20));
+            Assert.AreEqual(btnOut.RectangleToUIControl(r), new Rectangle(5, 15, 10, 20));
+            Assert.AreEqual(btnInner.RectangleToUIControl(r), new Rectangle(0, 0, 10, 20));
             //RectangleToUIWindow
             Assert.AreEqual(target.RectangleToUIWindow(r), r);
-            Assert.AreEqual(this.btnOut.RectangleToUIWindow(new Rectangle(5, 15, 10, 20)), r);
-            Assert.AreEqual(this.btnInner.RectangleToUIWindow(new Rectangle(0, 0, 10, 20)), r);
+            Assert.AreEqual(btnOut.RectangleToUIWindow(new Rectangle(5, 15, 10, 20)), r);
+            Assert.AreEqual(btnInner.RectangleToUIWindow(new Rectangle(0, 0, 10, 20)), r);
             //
-            this.btnOut.SendToBack();
-            this.btnOut.BringToFront();
+            btnOut.SendToBack();
+            btnOut.BringToFront();
         }
 
         private void btnWithBorder_Click(object sender, EventArgs e)
         {
-            this.TestCore(FormBorderStyle.Sizable, this);
+            TestCore(FormBorderStyle.Sizable, this);
         }
 
         private void btnNoBorder_Click(object sender, EventArgs e)
         {
-            this.TestCore(FormBorderStyle.None, this);
+            TestCore(FormBorderStyle.None, this);
         }
 
         private void btnWithBorder2_Click(object sender, EventArgs e)
         {
-            this.TestCore(FormBorderStyle.Sizable, this.uiWinControl1);
+            TestCore(FormBorderStyle.Sizable, uiWinControl1);
         }
 
         private void btnNoBorder2_Click(object sender, EventArgs e)
         {
-            this.TestCore(FormBorderStyle.None, this.uiWinControl1);
+            TestCore(FormBorderStyle.None, uiWinControl1);
         }
 
         private void btnAddFrame_Click(object sender, EventArgs e)
         {
-            this.image.AddFrame();
-            this.image.AddFrame();
-            this.image.AddFrame();
-            this.image.AddFrame();
-            this.image.AddFrame();
-            this.image.AddFrame();
+            image.AddFrame();
+            image.AddFrame();
+            image.AddFrame();
+            image.AddFrame();
+            image.AddFrame();
+            image.AddFrame();
         }
 
         private void btnClearFrame_Click(object sender, EventArgs e)
         {
-            this.image.ClearFrame();
+            image.ClearFrame();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.marquee.Stopped = !this.marquee.Stopped;
+            marquee.Stopped = !marquee.Stopped;
         }
     }
 }
